543a47783755c87ea7df88ce8e173c28e1b778ea
Administrator <admin@example.com>
2019-02-11 01:29:49 +0800

    新增日志拦截器

--- api/src/main/java/cn/itsite/jbase/test/TestContoller.java
+++ api/src/main/java/cn/itsite/jbase/test/TestContoller.java
@@ -1,49 +1,38 @@
 package cn.itsite.jbase.test;
 
 import cn.itsite.jbase.exception.MyException;
-import cn.itsite.jbase.dao.mapper.DictItemMapper;
+import cn.itsite.jbase.pojo.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
-@RestController        // @RestController = @Controller + @ResponseBody
+@RestController
 public class TestContoller {
     private static final Logger logger = LoggerFactory.getLogger(TestContoller.class);
 
-    @Autowired
-    private DictItemMapper dictItemMapper;
 
     @RequestMapping("/test")
-    public ResponseEntity getUser(@Valid String s) throws MyException {
+    public Resource getUser() throws MyException {
 ////        int i = 1 / 0;
 //
 ////        throw new MyException();
 //        List<DictItem> dictItems = dictItemMapper.selectAll();
-////        return "{\n" +
-////                "    \"test\": \"test\"\n" +
-////                "}";
-//
-//        for (DictItem dictItem : dictItems) {
+//         for (DictItem dictItem : dictItems) {
 //            logger.debug(dictItem.toString());
 //            System.out.println(dictItem.toString());
 //        }
-
-
-        return new ResponseEntity("11", HttpStatus.CREATED);
+        Resource resource = new Resource();
+        resource.setTest("11111111");
+        return resource;
+//        return new ResponseEntity("11", HttpStatus.CREATED);
     }
-
 
 
     public static void main(String[] args) {
         byte value = -1;
 //        String bs = String.format("%8s", Integer.toBinaryString(value));
-        String bs = String.format("%8s", Integer.toBinaryString(value&0xFF)).replace(" ", "0");
+        String bs = String.format("%8s", Integer.toBinaryString(value & 0xFF)).replace(" ", "0");
         System.out.println(Integer.toBinaryString(value));
     }
 }
